427fa8161294aa981118e9a8a26f68a541bf9db2
Than Htike Zaw <thanhtikezaw919@gmail.com>
2024-01-13 14:48:36 -0600

    added product validation

--- src/validation/products/productValidation.js
+++ src/validation/products/productValidation.js
@@ -3,7 +3,7 @@ const { body, validationResult } = require('express-validator');
 
 const productValidationRules = [
     body('product_name').notEmpty().withMessage('Product name is required'),
-    body('price').isFloat().withMessage('Price must be a  number'),
+    body('price').isFloat().withMessage('Price must be a  number').notEmpty().withMessage('price is required'),
     body('stock_quantity').isInt().withMessage('Stock quantity must be a valid integer'),
     body('description').notEmpty().withMessage('Description is required'),
     body('category_id').isInt().withMessage('Category ID must be a valid integer'),
