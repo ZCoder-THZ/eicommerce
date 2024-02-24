9d47f5a4522021b85241ffd308632ab2fd36e3a0
Than Htike Zaw <thanhtikezaw919@gmail.com>
2024-02-24 19:55:15 +0630

    added image uploading feature

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,22 +3,26 @@ const prisma = require('../../config/db.config');
 const { getItem, deleteItem } = require('../utils/itemUtils');
 const { retrivedSuccess, createSuccess } = require('../utils/jsonResponse');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // Specify the destination directory where uploaded files should be stored
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        // Specify how the filenames should be determined
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
 
-
+const upload = multer({ storage: storage });
 
 const getProducts = async (req, res) => {
     const data = await prisma.products.findMany({});
     return retrivedSuccess(res, 200, 'Products', data);
 };
-
 const createProduct = async (req, res, next) => {
     try {
-        // const errors = validationResult(req);
-
-        // if (!errors.isEmpty()) {
-        //     return res.status(400).json({ errors: errors.array() });
-        // }
-        // If validation passed, continue with the logic
         const { title, price, stock,brand_name,gender,product_image, description, category_id } = req.body;
 
         // Create the product in the database
@@ -43,5 +47,20 @@ const getProduct = (req, res) => {
          getItem(req, res, prisma.products);
 }
 
+const uploadImage = (req, res) => {
+    upload.single('file')(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            // A Multer error occurred when uploading.
+            return res.status(500).json({ error: err.message });
+        } else if (err) {
+            // An unknown error occurred.
+            return res.status(500).json({ error: 'An unknown error occurred',err });
+        }
 
-module.exports = { getProducts, createProduct, deleteProduct ,getProduct};
+        // File upload succeeded
+        res.status(200).json({ message: 'File uploaded successfully', file: req.file });
+    });
+};
+
+
+module.exports = { getProducts, createProduct, deleteProduct ,getProduct,uploadImage};
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,8 +1,9 @@
 const router=require('express').Router()
-const { getProducts, createProduct, deleteProduct, getProduct } = require('../controllers/productController')
+const { getProducts, createProduct, deleteProduct, getProduct,uploadImage } = require('../controllers/productController')
 const { productValidationRules } = require('../validation/products/productValidation')
 
 router.route('/').get(getProducts).post( createProduct)
+router.route('/imageUpload').post(uploadImage)
 router.route('/:id').delete(deleteProduct).get(getProduct)
 
 
